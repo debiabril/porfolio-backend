a7b80772ae42e38afb40a6ea4e7d2973b2923c35
Débora Abril Badosa <debora.badosa@gmail.com>
2023-04-22 15:24:50 -0300

    actualizando con el deploy del frontend

--- src/main/java/com/porfolio/dab/Security/Controller/AuthController.java
+++ src/main/java/com/porfolio/dab/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://dabfrontend.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://dabfrontend-c7e01.web.app","http://localhost:4200"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
